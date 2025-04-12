818db3dd0bb4054270d54d88a5fbd1fc59316967
Jose Quintas <jose.quintas@mga.com.pt>
2025-04-12 02:06:59 +0100

    Altera base de dados para Google Cloud - SQL Cloud

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua_chave_secreta_aqui'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///debug_adgest.db'
+    
+    # A string de conexão será definida através da variável de ambiente DATABASE_URL.
+    # Em desenvolvimento, se DATABASE_URL não estiver definida, você pode optar por usar SQLite;
+    # mas para produção, defina essa variável.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'icons')
     
